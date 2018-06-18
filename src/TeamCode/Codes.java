f1d90d1488e7f2890d6b98e173f0168dd7598025
narima asekova <nari_100591@mail.ru>
2018-06-18 12:18:23 -0700

    Narima

--- src/TeamCode/Codes.java
+++ src/TeamCode/Codes.java
@@ -1,9 +1,30 @@
 package TeamCode;
 
+import java.util.ArrayList;
+
 public class Codes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		int[] arr = new int[] { 1, 2,6,6,4, 3, 1, 2, 3, 4, 5 };
+
+		ArrayList<Integer> unique = new ArrayList<>();
+
+		 opp: for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+
+				if (arr[i] == arr[j] && i != j) {
+					continue opp;
+
+				} 
+
+			}
+			unique.add(arr[i]);
+		}
+		System.out.println(unique.toString());
+	}
+
+}
 
 	}
 
